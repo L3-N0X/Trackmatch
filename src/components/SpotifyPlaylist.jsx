7bfd7f2f086901e27f8b5300a882e78d03fd8180
Leon Gött <lotusleon@web.de>
2024-03-30 01:59:19 +0100

    working sorting table

--- src/components/SpotifyPlaylist.jsx
+++ src/components/SpotifyPlaylist.jsx
@@ -7,12 +7,11 @@ import {
   TableRow,
   TableCell,
   User,
-  Chip,
-  Tooltip,
-  getKeyValue
+  Spinner
 } from '@nextui-org/react';
-import SpotifySong from './SpotifySong';
+// import SpotifySong from './SpotifySong';
 import PropTypes from 'prop-types';
+import { useAsyncList } from '@react-stately/data';
 
 const columns = [
   { name: 'NAME', uid: 'name' },
@@ -20,54 +19,117 @@ const columns = [
   { name: 'LENGTH', uid: 'length' }
 ];
 
-export default function SpotifyPlaylist() {
-  const renderCell = React.useCallback((user, columnKey) => {
-    const cellValue = user[columnKey];
+const SpotifyPlaylist = ({ playlist }) => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  let tracklist = playlist.tracks.items;
+  let list = useAsyncList({
+    async load() {
+      setIsLoading(false);
 
+      return {
+        items: tracklist
+      };
+    },
+    async sort({ items, sortDescriptor }) {
+      return {
+        items: items.sort((a, b) => {
+          let first, second;
+
+          switch (sortDescriptor.column) {
+            case 'name':
+              first = a.track.name.toLowerCase();
+              second = b.track.name.toLowerCase();
+              break;
+            case 'artist':
+              first = a.track.artists[0].name.toLowerCase();
+              second = b.track.artists[0].name.toLowerCase();
+              break;
+            case 'length':
+              first = a.track.duration_ms;
+              second = b.track.duration_ms;
+              break;
+            default:
+              break;
+          }
+
+          let cmp = (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
+
+          if (sortDescriptor.direction === 'descending') {
+            cmp *= -1;
+          }
+
+          return cmp;
+        })
+      };
+    }
+  });
+  const renderCell = React.useCallback((track, columnKey) => {
     switch (columnKey) {
       case 'name':
         return (
           <User
-            avatarProps={{ radius: 'lg', src: user.avatar }}
-            description={user.email}
-            name={cellValue}>
-            {user.email}
+            key={track.id}
+            avatarProps={{ radius: 'lg', src: track.album.images[0].url }}
+            description={track.album.release_date}
+            name={track.name}>
+            {track.name}
           </User>
         );
       case 'artist':
         return (
-          <div className="flex flex-col">
-            <p className="text-bold text-sm capitalize">{cellValue}</p>
-            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          <div key={track.id} className="flex flex-col">
+            <p className="text-bold text-sm capitalize text-default-400">{track.artists[0].name}</p>
           </div>
         );
       case 'length':
-        return (
-          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
-            {cellValue}
-          </Chip>
-        );
+        return <p key={track.id}>{track.duration_ms / 1000 / 60}</p>;
       default:
-        return cellValue;
+        return <div key={track.id}>{track.name}</div>;
     }
   }, []);
-
+  console.log(list);
+  console.log(list.sortDescriptor);
   return (
-    <Table aria-label="Example table with custom cells">
+    <Table
+      isHeaderSticky
+      aria-label="Example table with custom cells"
+      selectionMode="single"
+      sortDescriptor={list.sortDescriptor}
+      onSortChange={list.sort}
+      classNames={{
+        base: 'max-h-[520px] overflow-y-scroll',
+        table: 'min-h-[420px]'
+      }}>
       <TableHeader columns={columns}>
         {(column) => (
-          <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+          <TableColumn
+            key={column.uid}
+            align={column.uid === 'actions' ? 'center' : 'start'}
+            allowsSorting>
             {column.name}
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody items={users}>
+      <TableBody
+        items={list.items}
+        isLoading={isLoading}
+        loadingContent={<Spinner label="Loading..." />}>
         {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          <TableRow key={item.track.name}>
+            {(columnKey) => (
+              <TableCell key={`${item.track.id}-${columnKey}`}>
+                {renderCell(item.track, columnKey)}
+              </TableCell>
+            )}
           </TableRow>
         )}
       </TableBody>
     </Table>
   );
-}
+};
+
+SpotifyPlaylist.propTypes = {
+  playlist: PropTypes.object.isRequired
+};
+
+export default SpotifyPlaylist;
